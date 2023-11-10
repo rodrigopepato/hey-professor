c348756a8b4c776857ac8fe8c5ade78f337344ed
Rodrigo Pepato <132705370+rodrigopepato@users.noreply.github.com>
2023-11-10 17:42:35 -0300

    HEY-9: it should have at least 10 characters when creating a question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -11,7 +11,7 @@ class QuestionController extends Controller
     {
 
         $attributes = request()->validate([
-            'question' => ['required'],
+            'question' => ['required', 'min:10'],
         ]);
 
         Question::query()->create($attributes);
--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -31,4 +31,21 @@ it('should check if ends with question mark ?', function () {
 
 it('should have at least 10 characters', function () {
 
+    //Arange :: preparar
+
+    $user = User::factory()->create();
+    actingAs($user);
+
+    //Act :: agir
+
+    $request = post(route('question.store'), [
+        'question' => str_repeat('*', 8) . '?',
+
+    ]);
+
+    //Assert :: verificar
+
+    $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
+    assertDatabaseCount('questions', 0);
+
 });
