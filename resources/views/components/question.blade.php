c50e3e6bb0deedc240b9e1871ea6404ccdf699d5
Rodrigo Pepato <132705370+rodrigopepato@users.noreply.github.com>
2023-11-16 18:47:51 -0300

    HEY-10: unlike button

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -16,7 +16,7 @@
             </button>
         </x-form>
 
-        <x-form :action="route('question.like', $question)">
+        <x-form :action="route('question.unlike', $question)">
             <button class="flex items-start space-x-1 text-red-500"> 
                 <x-icons.thumbs-down class="w-5 h-5 hover:text-red-300 cursor-pointer" />
                 <span>{{ $question->unlikes }}</span>
