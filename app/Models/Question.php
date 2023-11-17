3f3f3f8790c60d966c5350b09ef4aeaab59daee4
Rodrigo Pepato <132705370+rodrigopepato@users.noreply.github.com>
2023-11-17 16:02:24 -0300

    HEY-12: it should create as a draft all the time

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -10,6 +10,10 @@ class Question extends Model
 {
     use HasFactory;
 
+    protected $casts = [
+        'draft' => 'bool',
+    ];
+
     /**
      * @return HasMany<Vote>
      */
--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -6,41 +6,55 @@ use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, pos
 
 it('should be able to create a new question bigger than 255 characters', function () {
     //Arange :: preparar
-
     $user = User::factory()->create();
     actingAs($user);
 
     //Act :: agir
-
     $request = post(route('question.store'), [
         'question' => str_repeat('*', 260) . '?',
 
     ]);
 
     //Assert :: verificar
-
     $request->assertRedirect(route('dashboard'));
     assertDatabaseCount('questions', 1);
     assertDatabaseHas('questions', ['question' => str_repeat('*', 260) . '?']);
 
 });
 
-it('should check if ends with question mark ?', function () {
+it('should create as a draft all the time', function () {
 
     //Arange :: preparar
-
     $user = User::factory()->create();
     actingAs($user);
 
     //Act :: agir
+    post(route('question.store'), [
+        'question' => str_repeat('*', 260) . '?',
 
+    ]);
+
+    //Assert :: verificar
+    assertDatabaseHas('questions', [
+        'question' => str_repeat('*', 260) . '?',
+        'draft'    => true,
+    ]);
+
+});
+
+it('should check if ends with question mark ?', function () {
+
+    //Arange :: preparar
+    $user = User::factory()->create();
+    actingAs($user);
+
+    //Act :: agir
     $request = post(route('question.store'), [
         'question' => str_repeat('*', 10),
 
     ]);
 
     //Assert :: verificar
-
     $request->assertSessionHasErrors([
         'question' => 'Are you sure that this is a question? It is missing the question mark in the end',
     ]);
@@ -51,19 +65,16 @@ it('should check if ends with question mark ?', function () {
 it('should have at least 10 characters', function () {
 
     //Arange :: preparar
-
     $user = User::factory()->create();
     actingAs($user);
 
     //Act :: agir
-
     $request = post(route('question.store'), [
         'question' => str_repeat('*', 8) . '?',
 
     ]);
 
     //Assert :: verificar
-
     $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
     assertDatabaseCount('questions', 0);
 
