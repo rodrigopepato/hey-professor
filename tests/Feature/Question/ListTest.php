8e06b1e9b527b7bd69d6b9e6128b7919532a8dd9
Rodrigo Pepato <132705370+rodrigopepato@users.noreply.github.com>
2023-11-23 15:58:55 -0300

    HEY-17: ordering based on likes/unlikes

--- tests/Feature/Question/ListTest.php
+++ tests/Feature/Question/ListTest.php
@@ -28,14 +28,41 @@ it('should list all the question', function () {
 
 it('should paginate the result', function () {
 
-    $user      = User::factory()->create();
-    $questions = Question::factory()->count(20)->create();
+    $user = User::factory()->create();
+    Question::factory()->count(20)->create();
 
     actingAs($user);
 
     get(route('dashboard'))
         ->assertViewHas('questions', function ($value) {
             return $value instanceof LengthAwarePaginator;
+        });
+
+});
+
+it('should order by like and unlike, most liked question should be at the top, most unliked questions should be in the bottom', function () {
+
+    $user       = User::factory()->create();
+    $secondUser = User::factory()->create();
+    Question::factory()->count(5)->create();
+
+    $mostLikedQuestion   = Question::inRandomOrder()->find(3);
+    $mostUnlikedQuestion = Question::find(1);
+
+    $user->like($mostLikedQuestion);
+    $secondUser->unlike($mostUnlikedQuestion);
+
+    actingAs($user);
+
+    get(route('dashboard'))
+        ->assertViewHas('questions', function ($questions) {
+
+            expect($questions)
+                ->first()->id->toBe(3)
+                ->and($questions)
+                ->last()->id->toBe(1);
+
+            return true;
         });
 
 });
